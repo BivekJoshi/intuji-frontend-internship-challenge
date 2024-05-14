cf00b50c01b2ad1b25d5ca797e736e857b785e97
bivek.joshi <bivek.joshi@dghub.io>
2024-05-14 19:36:05 +0545

    my-first-commit

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const menuBtn = document.getElementById("menu-btn");
 const navLinks = document.getElementById("nav-links");
 const menuBtnIcon = menuBtn.querySelector("i");
 
+
 menuBtn.addEventListener("click", (e) => {
   navLinks.classList.toggle("open");
 
@@ -39,4 +40,62 @@ ScrollReveal().reveal(".header__content p", {
 ScrollReveal().reveal(".header__btns", {
   ...scrollRevealOption,
   delay: 2000,
+});
+
+ScrollReveal().reveal(".logo-1 img", {
+  ...scrollRevealOption,
+  origin: "right",
+  delay: 500,
+});
+ScrollReveal().reveal(".logo-2 img", {
+  ...scrollRevealOption,
+  origin: "right",
+  delay: 1000,
+});
+ScrollReveal().reveal(".logo-3 img", {
+  ...scrollRevealOption,
+  origin: "right",
+  delay: 1500,
+});
+ScrollReveal().reveal(".logo-4 img", {
+  ...scrollRevealOption,
+  origin: "right",
+  delay: 2000,
+});
+ScrollReveal().reveal(".logo-5 img", {
+  ...scrollRevealOption,
+  origin: "right",
+  delay: 2500,
+});
+ScrollReveal().reveal(".logo-6 img", {
+  ...scrollRevealOption,
+  origin: "right",
+  delay: 3000,
+});
+ScrollReveal().reveal(".logo-7 img", {
+  ...scrollRevealOption,
+  origin: "right",
+  delay: 3500,
+});
+
+ScrollReveal().reveal(".quote", {
+  ...scrollRevealOption,
+  origin: "bottom",
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".card-1", {
+  ...scrollRevealOption,
+  origin: "right",
+  delay: 3000,
+});
+ScrollReveal().reveal(".card-2", {
+  ...scrollRevealOption,
+  origin: "top",
+  delay: 3000,
+});
+ScrollReveal().reveal(".card-3", {
+  ...scrollRevealOption,
+  origin: "left",
+  delay: 3000,
 });
